a772eb160e3e8236fd8d4c99df4a3425a879d181
kennedylk <43650507+kennedylk@users.noreply.github.com>
2019-07-16 08:28:12 -0400

    Update averages.R

--- averages.R
+++ averages.R
@@ -6,20 +6,25 @@ library(gridExtra)
 library(dplyr)
 library(tidyr)
 library(openxlsx)}
-{data <- read_excel("X:/Reimbursement/RFPs/2018/Discounts/Tracking/By Region/Copy of Discount Tracking By Region KK.xlsx", 
+ #Read in file with data by state
+{data <- read_excel("Discount Tracking By Region.xlsx", 
                    range = "a3:Cn134")
-OOAdata<-read_excel("X:/Reimbursement/RFPs/2018/Discounts/Tracking/By Region/OOA Tracking.xlsx")
+#Read in file with OOA data
+ OOAdata<-read_excel("OOA Tracking.xlsx")
+#Melt, get rid of NA, delete unnecessary columns and rows
 dat<-melt(data)
 dat<-dat[is.na(dat$FCAST1),]
 dat<-dat[,c(1,2,8,9)]
 dat1<-dat[-c(1:860),]
 OOA<-melt(OOAdata)
+ #Make an identifier
 dat$id <- paste(dat$MEMSTATE, dat$PRODUCT, sep=":")
+#Separate bthe setlements into new data frames
 sttlmnt2015<-dat[grepl("Settlement 2015",dat$variable),]
 sttlmnt2016<-dat[grepl("Settlement 2016",dat$variable),]
 sttlmnt2017<-dat[grepl("Settlement 2017",dat$variable),]
 sttlmnt2018<-dat[grepl("Settlement 2018",dat$variable),]
-
+#Take non settlement years into df
 k15<-dat[grepl("2015",dat$variable),]
 k15$year<-c(2015)
 k16<-dat[grepl("2016",dat$variable),]
@@ -28,7 +33,7 @@ k17<-dat[grepl("2017",dat$variable),]
 k17$year<-c(2017)
 k18<-dat[grepl("2018",dat$variable),]
 k18$year<-c(2018)
-
+#Match up the discount for the settlements
 k15$Settlement <- sttlmnt2015$value[match(k15$id, sttlmnt2015$id)]
 k16$Settlement <- sttlmnt2016$value[match(k16$id, sttlmnt2016$id)]
 k17$Settlement <- sttlmnt2017$value[match(k17$id, sttlmnt2017$id)]
@@ -36,7 +41,7 @@ k18$Settlement <- sttlmnt2018$value[match(k18$id, sttlmnt2018$id)]
 dat<-rbind(k15,k16,k17,k18)
 dat<-dat[!grepl("Settlement",dat$variable),]
 
-
+#Add months in new column
 dat<-dat[,-c(5)]
 dat$value<-((dat$value)*100)
 dat$Settlement<-((dat$Settlement)*100)
@@ -65,6 +70,7 @@ dat$Settlement<-((dat$Settlement)*100)
   dat$month <- factor(dat$month, levels=c("Q1", "april", "may","june","july","aug","sept","oct","nov","dec"))
   
 }
+ #Calculate discount difference from end of the year settlement
 dat$diff<-(dat$value)-(dat$Settlement)
 attach(dat)
 k15<-dat[grepl("2015",dat$year),]
@@ -83,7 +89,7 @@ sept<-dat[grepl("Sept",dat$variable),]
 oct<-dat[grepl("Oct",dat$variable),]
 nov<-dat[grepl("Nov",dat$variable),]
 dec<-dat[grepl("Dec",dat$variable),]}
-
+#Find average difference in percentage from the end of the year
 #Q1
 {
   avgQ1M<-tapply(q1$diff,list(q1$MEMSTATE,q1$year),mean)
@@ -184,3 +190,18 @@ avgP+facet_grid(avgRegion$Var2~.)+ggtitle("Average Percentage Difference from Se
     labs(y="percentage difference from settlement on average", x = "Months") + theme(axis.text.x = element_text(angle = 40, hjust = 1))
   avgM+facet_grid(avgRegion$Var2~.)+ggtitle("Average Percentage Difference from Settlement by Region")+ theme(plot.title = element_text(hjust = 0.5))
 }
+#Boxplots
+
+prods<-ggplot(data =k15, aes(x=month,y=diff,group=PRODUCT,color=PRODUCT))+geom_line()+
+labs(y="difference(percent)", x = "Months") + theme(axis.text.x = element_text(angle = 40, hjust = 1))
+plotprod<-prods+facet_grid(MEMSTATE~.,scale="free_y")+ggtitle("Difference from Settlement in Percentage by Month")+ theme(plot.title = element_text(hjust = 0.5))
+plotprod  
+
+bp<-boxplot(diff~month,data=dat,main="Difference from Settlement - All Years", xlab="Months",ylab="Percent Difference from Settlement",abline(h=0))
+bp15<-boxplot(diff~month,data=k15,main="Difference from Settlement - 2015", xlab="Months",ylab="Percent Difference from Settlement",abline(h=0))
+bp16<-boxplot(diff~month,data=k16,main="Difference from Settlement - 2016", xlab="Months",ylab="Percent Difference from Settlement")
+bp17<-boxplot(diff~month,data=k17,main="Difference from Settlement - 2017", xlab="Months",ylab="Percent Difference from Settlement")
+bp18<-boxplot(diff~month,data=k18,main="Difference from Settlement - 2018", xlab="Months",ylab="Percent Difference from Settlement")
+lm1<-lm(diff~month-1,data=k15)
+summary(lm1)
+#write.xlsx(dat,'datafile.xlsx')
